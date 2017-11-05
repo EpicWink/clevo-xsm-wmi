1426651ba8ada92810eadbd43bf5a12e46f29fa5
Alexander Dießl <Alexander.Diessl@gmail.com>
2017-11-05 10:38:00 +0100

    Code formatting

--- module/clevo-xsm-wmi.c
+++ module/clevo-xsm-wmi.c
@@ -1573,13 +1573,13 @@ static struct dmi_system_id clevo_xsm_dmi_table[] __initdata = {
 		.driver_data = &kb_full_color_ops,
   },
 	{
-        .ident = "Clevo P65_67HSHP",
-        .matches = {
-            DMI_MATCH(DMI_PRODUCT_NAME, "P65_67HSHP"),
-        },
-        .callback = clevo_xsm_dmi_matched,
-        .driver_data = &kb_full_color_ops,
+    .ident = "Clevo P65_67HSHP",
+    .matches = {
+        DMI_MATCH(DMI_PRODUCT_NAME, "P65_67HSHP"),
     },
+    .callback = clevo_xsm_dmi_matched,
+    .driver_data = &kb_full_color_ops,
+  },
 	/* Ones that don't follow the 'standard' product names above */
 	{
 		.ident = "Clevo P7xxDM(-G)",
